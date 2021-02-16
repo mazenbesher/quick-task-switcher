4b5b15fb81f3ba3bd01ca148e7168686c7fe76f8
Mazen <mazenbesher@users.noreply.github.com>
2021-02-16 10:59:02 +0100

    show only desktops with more than one minute in pie

--- web/frontend/src/durations_pie.js
+++ web/frontend/src/durations_pie.js
@@ -1,19 +1,23 @@
 import Plotly from 'plotly.js/dist/plotly'
 import {seconds_to_str} from "./utils";
 
-export const create_desktops_durations_pie = (data, div) => {
-    var data = [{
-        values: data.map(info => info.duration_seconds),
-        labels: data.map(info => info.name),
+export const create_desktops_durations_pie = (desktops_info, div) => {
+    // filter to only desktop with durations more than one minute
+    const filtered_info = desktops_info.filter(info => info.duration_seconds >= 60)
+
+
+    const to_draw_data = [{
+        values: filtered_info.map(info => info.duration_seconds),
+        labels: filtered_info.map(info => info.name),
         type: 'pie',
-        text: data.map(info => `${info.name}<br>${seconds_to_str(info.duration_seconds)}`),
+        text: filtered_info.map(info => `${info.name}<br>${seconds_to_str(info.duration_seconds)}`),
         hoverinfo: 'text',
     }];
 
-    var layout = {
+    const layout = {
         height: 400,
         width: 500
     };
 
-    Plotly.newPlot(div, data, layout);
+    Plotly.newPlot(div, to_draw_data, layout);
 }
